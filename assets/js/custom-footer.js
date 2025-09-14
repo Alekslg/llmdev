b93003e4a1adb97e06b0c9d44ad971683e70acf1
Aleksandr Omelaev <omelaev.alexandr@sbps.ru>
2025-09-14 12:16:35 +0300

    Deployed 8a25ba7 with MkDocs version: 1.6.1

--- assets/js/custom-footer.js
+++ assets/js/custom-footer.js
@@ -1,12 +1,24 @@
+const translations = {
+  en: "<strong>Made with love for Python</strong><br>Writing to remember. Sharing to help.",
+  ru: "<strong>Создано с любовью к Python</strong><br>Пишу, чтобы запомнить. Делюсь, чтобы помочь.",
+};
+
+const lang = document.documentElement.lang || "en";
+const TARGET_FOOTER_TEXT = translations[lang] || translations["en"];
+
 function updateFooter() {
   const copyrightDiv = document.querySelector(".md-copyright");
-  if (copyrightDiv) {
-    copyrightDiv.innerHTML = `
-            <strong>Создано с любовью к Python</strong><br>
-            Пишу, чтобы запомнить. Делюсь, чтобы помочь.
-        `;
-  }
+  if (!copyrightDiv) return;
+
+  if (copyrightDiv.innerHTML.trim() === TARGET_FOOTER_TEXT.trim()) return;
+
+  copyrightDiv.innerHTML = TARGET_FOOTER_TEXT;
 }
 
 document.addEventListener("DOMContentLoaded", updateFooter);
-document.addEventListener("mkdocs:page-loaded", updateFooter);
+
+document.addEventListener("click", function (e) {
+  if (e.target.closest("a")) {
+    setTimeout(updateFooter, 100);
+  }
+});
